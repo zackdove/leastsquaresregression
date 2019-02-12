cf0346f7006d4f6a135f9645630f71966730dea6
zackdove <zdove@live.co.uk>
2019-02-12 18:00:03 +0000

    added segmentation

--- lsr.py
+++ lsr.py
@@ -15,6 +15,13 @@ def load_points_from_file(filename):
     points = pd.read_csv(filename, header=None)
     return points[0].values, points[1].values
 
+def getlinesegments(xs, ys):
+    size = len(xs)
+    segmentedxs = [xs[i:i+20] for i in range(0, size, 20)]
+    segmentedys = [ys[i:i+20] for i in range(0, size, 20)]
+    segments = size//20
+    return segmentedxs, segmentedys, segments
+
 
 def view_data_segments(xs, ys):
     """Visualises the input file with each segment plotted in a different colour.
@@ -35,24 +42,39 @@ def view_data_segments(xs, ys):
 
 def calculatelsr(xs, ys):
     #do calculations
-    print("unimplimented")
+    XT = np.matrix([np.ones(20),xs])
+    X = XT.getT()
+    Y = np.matrix(ys).getT()
+    print(XT)
+    print(X)
+    A = ((XT * X).getI() * XT) * Y
+    print(A)
 
 def main(argv):
-    if (len(argv) > 3 or len(argv)==0 ):
+    if (len(argv) > 3 or len(argv)==1 ):
         print("Error. Incorrect format.")
         exit()
+
+    #Check if input file exists
     if (not os.path.isfile(argv[1])):
         print("Error. File does not exist.")
         exit()
 
     #Get xs, ys from file specified in command line call
-    xs, ys = load_points_from_file(argv[1])
+    filename = argv[1]
+    xs, ys = load_points_from_file(filename)
+    segmentedxs, segmentedys, segments = getlinesegments(xs, ys)
+
     #Send xs, ys to calculatelsr()
-    calculatelsr(xs, ys)
+    for i in range(segments):
+        calculatelsr(segmentedxs[i], segmentedys[i])
 
     if (len(argv) == 3 and argv[2]=="--plot"):
         print("Plot detected")
         #do plot stuff
+        view_data_segments(xs, ys)
+
+
 
 
 if __name__ == "__main__":
